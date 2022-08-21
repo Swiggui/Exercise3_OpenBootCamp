b14e95e1bb446cf2dac72c6595f686cd56205645
Swiggui <figueredojuan2020@outlook.es>
2022-08-21 17:26:34 -0500

    Proyecto Tema 3
    Primera parte:
    
    Crear una función con tres parámetros que sean números que se suman entre sí.
    
    Llamar a la función en el main y darle valores.
    
    Segunda parte:
    
    Crear una clase coche.
    
    Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
    
    Una función que incremente el número de puertas que tiene el coche.
    
    Crear un objeto miCoche en el main y añadirle una puerta.
    
    Mostrar el número de puertas que tiene el objeto.

---
+++ untitled/src/coche.java
@@ -0,0 +1,7 @@
+public class coche {
+    static int numPuertas = 0;
+    public static int increaseDoors(){
+        numPuertas += 1;
+        return numPuertas;
+    }
+}
--- untitled/src/main.java
+++ untitled/src/main.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class main {
     static int[] numero;
-    static int zuma;
+    static int zuma, doors, option;
     static Scanner key = new Scanner(System.in);
     public static void main(String[] args) {
         numero = new int[3];
         numero = intro();
         zuma = suma(numero);
-        System.out.println("La suma de los números " + Arrays.toString(numero) + " es: " + zuma);
+        System.out.println("La suma de los números " + Arrays.toString(numero) + "es: " + zuma);
+        //Parte 2
+        cars();
     }
     public static int[] intro(){
         for (int i=0; i< numero.length;i++){
@@ -23,5 +25,19 @@ public class main {
             zuma += numero[i];
         }
         return zuma;
+    }
+
+    public static void cars() {
+        System.out.println("Puertas del auto");
+        while (true){
+            coche miCoche = new coche();
+            doors = miCoche.increaseDoors();
+            System.out.println("El coche tiene " + doors + " puertas.");
+            System.out.println("¿Desea añadir mas puertas? \n1.Si\n2.No");
+            option = key.nextInt();
+            if (option != 1){
+                break;
+            }
+        }
     }
 }
